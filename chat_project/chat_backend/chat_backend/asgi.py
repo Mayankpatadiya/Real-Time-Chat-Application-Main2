16593e6e92aaf789f02d527a7f5d0ec17febe608
Mayank Patadiya <mayankpatadiya@28gmail.com>
2026-02-28 22:03:55 +0530

    Ready to deploy

--- chat_project/chat_backend/chat_backend/asgi.py
+++ chat_project/chat_backend/chat_backend/asgi.py
@@ -1,20 +1,21 @@
 import os
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_backend.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "chat_backend.settings"
+)
 
-django.setup()
+django.setup()  # ✅ MUST come BEFORE importing routing
 
-import chat.routing  # <-- import AFTER setup
+from chat.routing import websocket_urlpatterns  # ✅ Import AFTER setup
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+    "websocket": URLRouter(
+        websocket_urlpatterns
     ),
 })
