73baab0c31a3e674b863cace5a056c0146712199
Mayank Patadiya <mayankpatadiya@28gmail.com>
2026-03-01 11:41:32 +0530

    __

--- chat_project/chat_backend/chat_backend/asgi.py
+++ chat_project/chat_backend/chat_backend/asgi.py
@@ -3,6 +3,7 @@ import django
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
