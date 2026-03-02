dd3cc56b0f3860a5ee33296dd2bf743a249f7ba9
Mayankpatadiya <mayankpatadiya28@gmail.com>
2026-03-02 11:29:41 +0530

    _

--- chat_project/chat_backend/chat/views.py
+++ chat_project/chat_backend/chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render , redirect, get_object_or_404
 from django.db import models
 from django.http import HttpResponse, JsonResponse
 from django.http import HttpRequest
