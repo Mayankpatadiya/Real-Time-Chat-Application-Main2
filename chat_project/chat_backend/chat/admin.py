276dd11c14a5f574ed24d65e30634aeaef719178
Mayank Patadiya <mayankpatadiya@28gmail.com>
2026-03-19 23:47:51 +0530

    solve admin panel error

--- chat_project/chat_backend/chat/admin.py
+++ chat_project/chat_backend/chat/admin.py
@@ -17,9 +17,10 @@ class MessageAdmin(admin.ModelAdmin):
 
 @admin.register(ChatGroup)
 class ChatGroupAdmin(admin.ModelAdmin):
-    list_display = ('id','name','created_by','created_at')
-    list_filter = ('name','created_by','created_at')
-    search_fields = ('name', 'created_by__user__username')
+    list_display= ('id','name','created_by','created_at')
+    list_filter = ('created_by','created_at')
+    filter_horizontal = ('members',)
+    search_fields = ('name', 'members__user__username', 'created_by__user__username')
 
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
